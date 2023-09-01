71d77c53283c376ab958c6f72db1a715cb144a80
Chobby <kwb020312@naver.com>
2023-09-01 23:59:41 +0900

    Feat: Naver 실시간 검색어 호출

--- app.js
+++ app.js
@@ -1,9 +1,69 @@
 const express = require("express");
+const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
+const { NAVER_SHOPPING_BEST_KEYWORD } = require("./assets/constant/naver");
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", async (req, res) => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    page.setDefaultNavigationTimeout(90000);
+    await page.goto(NAVER_SHOPPING_BEST_KEYWORD, { waitUntil: "networkidle0" });
+
+    // 페이지의 HTML을 가져옵니다.
+    const content = await page.content();
+
+    // Puppeteer로 가져온 HTML 컨텐츠에 Cheerio를 사용합니다.
+    const $ = cheerio.load(content);
+    const result = [];
+
+    // 네이버 인기순위 접근
+    $(".list_keyword > li > a")
+      .contents()
+      .filter(function () {
+        return this.type === "text";
+      })
+      .each(async function (i, _) {
+        const curText = $(this).text().trim();
+        result[i] = { keyword: curText, items: [] };
+        if (i !== 0) {
+          await page.waitForSelector("[class^='chartList_btn_keyword']");
+          const openNextItem = await page.$$(
+            "[class^='chartList_btn_keyword']"
+          );
+          await openNextItem[i].evaluate((b) => b.click());
+          await page.waitForNavigation();
+
+          await page.waitForSelector('[class^="chartList_text_area"]');
+          const getItemData = await page.$('[class^="chartList_text_area"]');
+          const item = await getItemData.evaluate((b) => b.innerHTML);
+          const exceptTag = item.replace(/<\/?[^>]+(>|$)/g, "");
+          const [value, key] = exceptTag.split("원");
+          result[i].items.push({ title: key, price: value + "원" });
+        } else {
+          $('[class^="chartList_text_area"]').each(function () {
+            const key = $(this)
+              .find("[class^='chartList_title']")
+              .text()
+              .trim();
+            const value = $(this)
+              .find("[class^='chartList_price']")
+              .text()
+              .trim();
+            result[i].items.push({ title: key, price: value });
+          });
+        }
+      });
+
+    // await browser.close();
+    res.status(200).send(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error occurred during crawling.");
+  }
 });
 
 app.listen(port, () => {
---
+++ assets/constant/naver.js
@@ -0,0 +1,6 @@
+const NAVER_SHOPPING_BEST_KEYWORD =
+  "https://search.shopping.naver.com/best/category/keyword?categoryCategoryId=ALL&categoryDemo=A00&categoryRootCategoryId=ALL&chartRank=1&period=P1D";
+
+module.exports = {
+  NAVER_SHOPPING_BEST_KEYWORD,
+};
